2ebef80df22f169a52f25b70361d7ab7f1dafa83
Erwing Coss <erwingcoss21@hotmail.com>
2024-07-04 09:40:41 -0400

    Interface created to comunicate the repository with the controller.

---
+++ CodePulse/API/CodePulse.API/CodePulse.API/Repository/Interface/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using CodePulse.API.Models.Domain;
+
+namespace CodePulse.API.Repository.Interface
+{
+    public interface ICategoryRepository
+    {
+        /*This interface its going to be implemented on the categoryRepository class.*/
+        Task<Category> CreateAsync(Category category);
+    }
+}
