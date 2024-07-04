e70a71db311e8fd1eb7251970199be417096e386
Erwing Coss <erwingcoss21@hotmail.com>
2024-07-04 09:39:09 -0400

    Category repository, adding a method to create a resource asynchronous

---
+++ CodePulse/API/CodePulse.API/CodePulse.API/Repository/Implementation/CategoyRepository.cs
@@ -0,0 +1,29 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Repository.Implementation
+{
+    public class CategoyRepository : ICategoryRepository
+    {
+        /*
+         in the repository we work directlty we the domain model. 
+        Because this layer works as an access layer to the data 
+        so its better and a good practice to use the domains directly here 
+        cause we are working directly with the data.
+         */
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoyRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;     
+        }
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+            return category;
+        }
+    }
+}
