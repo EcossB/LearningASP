6cbe004b1b4efd6cf91edb05fa9f2bdc941dbe04
Erwing Coss <erwingcoss21@hotmail.com>
2024-07-04 09:37:08 -0400

    Categories controller created.

---
+++ CodePulse/API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO.CategoryDtos;
+using CodePulse.API.Repository.Implementation;
+using CodePulse.API.Repository.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this._categoryRepository = categoryRepository;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            //mapping dto to domaing model
+            var category = new Category()
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+            
+            await _categoryRepository.CreateAsync(category);
+
+            //mapping domain model to Dto response
+            var response = new CategoryResponseDto()
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+    }
+}
