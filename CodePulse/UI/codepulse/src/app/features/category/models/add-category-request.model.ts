f0a3cb4eacdbbc89874a563269d79c01381640a2
Erwing Coss <erwingcoss21@hotmail.com>
2024-07-10 17:37:54 -0400

    Models to make request and get data.

---
+++ CodePulse/UI/codepulse/src/app/features/category/models/add-category-request.model.ts
@@ -0,0 +1,7 @@
+/**
+ * entiendo que se puede declarar el archivo como interface. porque a nivel de compilacion las interfaces no guardan espacio en memoria.
+ */
+export interface AddCategoryRequest {
+  Name: string;
+  UrlHandle: string;
+}
