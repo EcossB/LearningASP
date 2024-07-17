658d08174694d3e7e6cf60b8e44599e82be7bb43
Erwing Coss <erwingcoss21@hotmail.com>
2024-07-17 08:06:17 -0400

    Dto for update a category request

---
+++ CodePulse/API/CodePulse.API/CodePulse.API/Models/DTO/CategoryDtos/UpdateCategoryRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CodePulse.API.Models.DTO.CategoryDtos
+{
+    public class UpdateCategoryRequestDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
