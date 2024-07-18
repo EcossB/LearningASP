dd30053af06925d33eaf9617c57056108f58dcb3
Erwing Coss <erwingcoss21@hotmail.com>
2024-07-18 17:27:17 -0400

    creating blogpost repository

---
+++ CodePulse/API/CodePulse.API/CodePulse.API/Repository/Interface/IBlogPostRepository.cs
@@ -0,0 +1,11 @@
+﻿using CodePulse.API.Models.Domain;
+
+namespace CodePulse.API.Repository.Interface
+{
+    public interface IBlogPostRepository
+    {
+        public Task<BlogPost> CreateAsync(BlogPost blogPost);
+
+        public Task<IEnumerable<BlogPost>> GetAllAsync();
+    }
+}
