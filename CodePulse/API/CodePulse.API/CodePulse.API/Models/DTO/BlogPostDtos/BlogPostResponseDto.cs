932cf161afe02dd7b431e28952eb13f00b2b6259
Erwing Coss <erwingcoss21@hotmail.com>
2024-07-29 16:23:56 -0400

    Creating a response when creating a new blogpost. a editblog request dto and up

---
+++ CodePulse/API/CodePulse.API/CodePulse.API/Models/DTO/BlogPostDtos/BlogPostResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using CodePulse.API.Models.DTO.CategoryDtos;
+
+namespace CodePulse.API.Models.DTO.BlogPostDtos
+{
+    public class BlogPostResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Content { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool IsVisible { get; set; }
+        public List<CategoryResponseDto> Category { get; set; } = new List<CategoryResponseDto>();
+    }
+}
