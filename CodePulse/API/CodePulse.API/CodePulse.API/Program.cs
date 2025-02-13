cd362397596368e7dbbeac5deac3b602c1db79d5
EcossB <erwingcoss21@hotmail.com>
2025-02-13 19:47:47 -0400

    adding contextaccesor, adding new dbcontext, adding new scope, and adding authentication and authorization. and lastly static file options

--- CodePulse/API/CodePulse.API/CodePulse.API/Program.cs
+++ CodePulse/API/CodePulse.API/CodePulse.API/Program.cs
@@ -1,13 +1,19 @@
+using System.Text;
 using CodePulse.API.Data;
 using CodePulse.API.Repository.Implementation;
 using CodePulse.API.Repository.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,10 +24,55 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CodePulseConnectionString"));
 });
 
+// injecting AuthDbContext. 
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CodePulseConnectionString"));
+});
+
+
+
 builder.Services.AddScoped<ICategoryRepository, CategoyRepository>();
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
+builder.Services.AddScoped<IImageRepository, ImageRepository>();
+builder.Services.AddScoped<ITokenRepository, TokenRepository>();
 
 
+//injecting the identityUser
+builder.Services.AddIdentityCore<IdentityUser>() //what type of user we are using
+    .AddRoles<IdentityRole>() //what type of role we are going to use
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("CodePulse") 
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+// we this we set how the password can come to our api
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
+
+// creando la autenticacion de la aplicacion
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => //agregamos el jwt bearer 
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters //aqui configuramos los parametros que tendra el token
+            {
+                AuthenticationType = "Jwt", //aqui le decimos del tipoque es
+                ValidateIssuer = true, //aqui le decimos que valide quien firma el token que somos nosotros
+                ValidateAudience = true, // aqui validamos a la persona que va, que en este caso es el frontend
+                ValidateLifetime = true, //aqui le ponemos que valide si el token expiro por el tiempo
+                ValidateIssuerSigningKey = true, // aqui que valide la llave definida en el appsettings
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+            };
+    });
 
 var app = builder.Build();
 
@@ -41,7 +92,16 @@ app.UseCors(options =>
     options.AllowAnyOrigin();
 });
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+
+//De esta manera la api puede usar archivos estaticos!
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+    RequestPath = "/Images"
+});
 
 app.MapControllers();
 
