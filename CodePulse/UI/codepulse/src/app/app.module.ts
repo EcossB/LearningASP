5d48d7c42a56091ca16a129cb2f05f4eacd894ea
EcossB <erwingcoss21@hotmail.com>
2025-03-05 23:15:06 -0400

    adding new components and interceptos

--- CodePulse/UI/codepulse/src/app/app.module.ts
+++ CodePulse/UI/codepulse/src/app/app.module.ts
@@ -7,12 +7,17 @@ import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
 import { AddCategoryComponent } from './features/category/add-category/add-category.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
 import { BlogpostListComponent } from './features/blogpost/blogpost-list/blogpost-list.component';
 import { AddBlogpostComponent } from './features/blogpost/add-blogpost/add-blogpost.component';
 import { MarkdownModule } from 'ngx-markdown';
 import { EditBlogpostComponent } from './features/blogpost/edit-blogpost/edit-blogpost.component';
+import { ImageSelectorComponent } from './shared/components/image-selector/image-selector.component';
+import { HomeComponent } from './features/public/home/home.component';
+import { BlogDetailsComponent } from './features/public/blog-details/blog-details.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { AuthInterceptor } from './core/components/interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -23,7 +28,11 @@ import { EditBlogpostComponent } from './features/blogpost/edit-blogpost/edit-bl
     EditCategoryComponent,
     BlogpostListComponent,
     AddBlogpostComponent,
-    EditBlogpostComponent
+    EditBlogpostComponent,
+    ImageSelectorComponent,
+    HomeComponent,
+    BlogDetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +41,11 @@ import { EditBlogpostComponent } from './features/blogpost/edit-blogpost/edit-bl
     HttpClientModule,
     MarkdownModule.forRoot()
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
