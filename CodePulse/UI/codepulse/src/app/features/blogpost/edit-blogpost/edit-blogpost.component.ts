43243de3f8b529ae93338768f85b54ea56411e03
EcossB <erwingcoss21@hotmail.com>
2025-03-05 23:16:31 -0400

    refactoring edit blogpost

--- CodePulse/UI/codepulse/src/app/features/blogpost/edit-blogpost/edit-blogpost.component.ts
+++ CodePulse/UI/codepulse/src/app/features/blogpost/edit-blogpost/edit-blogpost.component.ts
@@ -7,6 +7,7 @@ import { Category } from '../../category/models/category.model';
 import { CategoryService } from '../../category/services/category.service';
 import { UpdateCategoryRequest } from '../../category/models/update-category-request.model';
 import { EditBlogPostRequest } from '../model/edit-blogpost-request.model';
+import { ImageService } from '../../../shared/components/image-selector/image.service';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -20,9 +21,13 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
   idsubscription$?: Subscription;
   updateBlogSubscription?: Subscription;
   getBlogSubscription?: Subscription;
+  imageSubscription?: Subscription;
+
+
   BlogPost?: BlogPost;
   Categories$?: Observable<Category[]>;
   selectedCategories?: string[];
+  imagesSelectorVisible: boolean = false;
 
   /**
    * @route to get the id from the url.
@@ -30,7 +35,8 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
   constructor(private route: ActivatedRoute,
               private BlogPostService: BlogPostService,
               private categoryService: CategoryService,
-              private router: Router
+              private router: Router,
+              private imageService: ImageService
   ) {
 
   }
@@ -51,6 +57,14 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
           })
         }
 
+       this.imageSubscription = this.imageService.onSelectImage().subscribe({
+            next: (response) => {
+              if (this.BlogPost){
+                this.BlogPost.featuredImageUrl = response.url;
+                this.imagesSelectorVisible = false;
+              }
+            }
+          })
       }
     })
   }
@@ -60,17 +74,16 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
     if(this.BlogPost && this.id){
 
       const updateBlog : EditBlogPostRequest = {
-        title: this.BlogPost.title ?? '',
-        shortDescription: this.BlogPost.shortDescription ?? '',
-        content: this.BlogPost.content ?? '',
-        featuredImageUrl: this.BlogPost.featuredImageUrl ?? '',
-        urlHandle: this.BlogPost.urlHandle ?? '',
+        title: this.BlogPost.title,
+        shortDescription: this.BlogPost.shortDescription ,
+        content: this.BlogPost.content ,
+        featuredImageUrl: this.BlogPost.featuredImageUrl ,
+        urlHandle: this.BlogPost.urlHandle ,
         publishedDate: this.BlogPost.publishedDate,
-        author: this.BlogPost.author ?? '',
-        isVisible: this.BlogPost.isVisible ?? false,
+        author: this.BlogPost.author,
+        isVisible: this.BlogPost.isVisible,
         category: this.selectedCategories ?? []
       }
-
 
       this.updateBlogSubscription = this.BlogPostService.updatePost(this.id, updateBlog).subscribe({
         next: () => {
@@ -81,10 +94,32 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
     }
   }
 
+  onDeleteBlog(): void {
+    if(this.id){
+      this.BlogPostService.deletePostById(this.id).subscribe({
+        next: () => {
+          console.log("BlogPost deleted");
+          this.router.navigateByUrl('/admin/blogpost');
+        }
+      })
+    }
+  }
+
+  openImageSelector(): void {
+    //togglin the model pop up.
+    this.imagesSelectorVisible = true;
+  }
+
+  closeImageSelector(): void {
+    this.imagesSelectorVisible = false;
+  }
+
+
   ngOnDestroy(): void {
     this.idsubscription$?.unsubscribe();
     this.updateBlogSubscription?.unsubscribe();
     this.getBlogSubscription?.unsubscribe();
+    this.imageSubscription?.unsubscribe();
   }
 
 
