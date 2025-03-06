572b2284ea4c8c9fa8ecb44f94287cba0ec8f615
EcossB <erwingcoss21@hotmail.com>
2025-03-05 23:15:20 -0400

    adding navbar changes

--- CodePulse/UI/codepulse/src/app/core/components/navbar/navbar.component.ts
+++ CodePulse/UI/codepulse/src/app/core/components/navbar/navbar.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../features/auth/services/auth.service';
+import { userModel } from '../../../features/auth/Models/user.models';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  user?: userModel;
+
+  constructor(private auth: AuthService,
+              private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.auth.user().subscribe({
+      next: (data) => {
+        this.user = data;
+      }
+    });
+
+   this.user = this.auth.getUser();
+  }
+
+  logOut(): void {
+    this.auth.logOut();
+    this.router.navigateByUrl('/');
+  }
+
 
 }
